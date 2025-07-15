65fc831a0679db43c7caad0290dc8df87aa09973
srikanth <srikanth.baireddy@outlook.com>
2025-07-15 10:44:14 +0530

    terraform

--- main.tf
+++ main.tf
@@ -8,5 +8,6 @@ environment = var.environment
 public_subnet_cidrs = var.public_subnet_cidrs
 private_subnet_cidrs = var.private_subnet_cidrs
 database_subnet_cidrs = var.database_subnet_cidrs
+is_peering_required = true
 
 }
