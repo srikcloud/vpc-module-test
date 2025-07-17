167f71dc2bcafc5c6bd69f06cced8ca6f10917c4
srikanth <srikanth.baireddy@outlook.com>
2025-07-17 23:43:23 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "../terraform-aws-vpc"
+source = "git::https://github.com/srikcloud/terraform-aws-vpc.git?ref=main"
 /*   project = "roboshop"
   environment = "dev"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
